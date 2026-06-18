3247f927a32588287ea10c051e1fe88777a39796
Gustavo1137 <167646327+Gustavo1137@users.noreply.github.com>
2026-06-18 01:22:57 -0300

    Cambios en el TP agregar DMA y UART

--- TP-Final/TP.c
+++ TP-Final/TP.c
@@ -10,9 +10,9 @@
 #include "lpc17xx_exti.h"
 #include "lpc17xx_uart.h"
 #include <cr_section_macros.h>
-#include <stdint.h>
-#include <string.h>         // ← NUEVO (para strcmp)
-#include <stdio.h>          // ← NUEVO (para sprintf)
+//#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
 
 #define CONTROL 1000
 #define CONTROL1 1000
@@ -24,9 +24,11 @@ volatile int PWM1 = 0;
 volatile int PWM2 = 0;
 volatile uint16_t convertido1 = 0;
 volatile uint16_t convertido2 = 0;
-uint32_t* Buffer = (uint32_t*) 0x2007C000;//banco 0
-uint32_t* PosicionesGuar1 = (uint32_t*) 0x20080000;//primera mitad del banco 1
-uint32_t* PosicionesGuar2 = (uint32_t*) 0x20082000;//segunda mitad del banco
+uint32_t BufferDMA[1024];
+//uint16_t BufferServo1[512];
+//uint16_t BufferServo2[512];
+//uint32_t* PosicionesGuar1 = (uint32_t*) 0x20080000;//primera mitad del banco 1
+//uint32_t* PosicionesGuar2 = (uint32_t*) 0x20082000;//segunda mitad del banco
 volatile uint16_t totalPosiciones = 0;  // cuántas se guardaron
 volatile uint16_t idxAuto = 0;          // cuál se reproduce ahora
 volatile char rx_buf[16];
@@ -36,52 +38,31 @@ volatile uint8_t modo = MODO_MANUAL;
 volatile int angulo = 0;
 volatile uint16_t angulo1 = 0;
 volatile uint16_t angulo2 = 0;
+volatile uint16_t i1 = 0;
+volatile uint16_t i2 = 0;
+volatile uint16_t t1 = 0;
+volatile uint16_t t2 = 0;
+volatile int index=0;
+uint16_t adc=0;
+uint8_t canal=0;
+uint32_t dato = 0;
+uint32_t valorUART = 0;
+uint32_t Estado = 1;
 
 
-/*static int int2str(char* buf, int val) {
+GPDMA_LLI_T lli;
+
+
+static int itoa_simple(int val, char* buf) {
     int i = 0;
-    if(val < 0){ buf[i++] = '-'; val = -val; }
-    if(val == 0){ buf[i++] = '0'; return i; }
+    if (val == 0) { buf[i++] = '0'; buf[i] = '\0'; return i; }
     char tmp[10]; int t = 0;
-    while(val > 0){ tmp[t++] = '0' + (val % 10); val /= 10; }
-    while(t > 0){ buf[i++] = tmp[--t]; }
+    while (val > 0) { tmp[t++] = '0' + (val % 10); val /= 10; }
+    while (t > 0) buf[i++] = tmp[--t];
+    buf[i] = '\0';
     return i;
-}*/
-
-void configPIN(void);
-void confiEINT(void);
-void configADC(void);
-void configDAC(void);
-void configDMA(void);
-void configUART(void);
-void enviarUART(void);
-void configTimer(void);
-void calcularPWMServo1(void);
-void calcularPWMServo2(void);
-void enviarUART(void);
-void configGPIO_UART(void);
-
-int main(){
-	/*GPDMA_LLI_T lli;
-	lli.srcAddr = (uint32_t)&LPC_ADC->ADGDR;
-	lli.dstAddr = (uint32_t)&Buffer;
-	lli.nextLLI = lli;
-	lli.control = 4096 | (2 << 18) | (2 << 21) | (1 << 27);*/
-	configPIN();
-	confiEINT();
-	configADC();
-	configDAC();
-	configDMA();
-	configUART();
-	//configGPIO_UART();
-	configTimer();
-	calcularPWMServo1();
-	calcularPWMServo2();
-	//char msg[] = "100";
-	while(1){
-
-	}
 }
+
 
 void configPIN(){
 	//CONFIGURO EL PIN P0.23, PARA EL ADC
@@ -181,10 +162,10 @@ void configTimer(){
 	TIM_InitTimer(LPC_TIM0, &timerCfg);
 
 	//CONFIGURAR EL TIMER1
-	TIM_TIMERCFG_T timerCfg1;
+	/*TIM_TIMERCFG_T timerCfg1;
 	timerCfg1.prescaleOpt = TIM_US;
 	timerCfg1.prescaleValue = 1;
-	TIM_InitTimer(LPC_TIM1, &timerCfg1);
+	TIM_InitTimer(LPC_TIM1, &timerCfg1);*/
 
 	//CONFIGURAR EL TIMER2
 	TIM_TIMERCFG_T timerCfg1p;
@@ -232,25 +213,13 @@ void configTimer(){
 	matchCfg1s.matchValue = 20000;
 	TIM_ConfigMatch(LPC_TIM2, &matchCfg1s);
 
-	//CONFIGURAR EL MATCH0 DEL TIMER1
-	TIM_MATCHCFG_T matchCfg2;
-	matchCfg2.channel = TIM_MATCH_0;
-	matchCfg2.intEn = ENABLE;
-	matchCfg2.stopEn = DISABLE;
-	matchCfg2.resetEn = ENABLE;
-	matchCfg2.extOpt = TIM_NOTHING;
-	matchCfg2.matchValue = 250000;
-	TIM_ConfigMatch(LPC_TIM1, &matchCfg2);
-
 	GPIO_SetPins(PORT_0,(1<<0));
 	GPIO_SetPins(PORT_0,(1<<1));
 
 	//INTERRUPCIONES
 	NVIC_EnableIRQ(TIMER0_IRQn);
-    NVIC_EnableIRQ(TIMER1_IRQn);
     NVIC_EnableIRQ(TIMER2_IRQn);
     TIM_Enable(LPC_TIM0);
-    TIM_Enable(LPC_TIM1);
     TIM_Enable(LPC_TIM2);
 }
 
@@ -265,6 +234,8 @@ void configADC(){
     ADC_ChannelEnable(ADC_CHANNEL_3);
 
     ADC_PowerUp();
+
+    ADC_BurstEnable();
 }
 
 void confiEINT(){
@@ -289,8 +260,154 @@ void confiEINT(){
 	NVIC_EnableIRQ(EINT1_IRQn);
 }
 
-void configGPIO_UART(void) {
-    //nada
+
+
+void configUART(void) {
+
+    // ← ESTO FALTABA: configurar P0.2 (TXD0) y P0.3 (RXD0)
+    PINSEL_CFG_T pinCfg;
+
+    pinCfg.port   = PORT_0;
+    pinCfg.pin    = PIN_2;
+    pinCfg.func   = PINSEL_FUNC_01;   // funcion UART0 TXD
+    pinCfg.mode   = PINSEL_PULLUP;
+    pinCfg.openDrain = DISABLE;
+    PINSEL_ConfigPin(&pinCfg);
+
+    pinCfg.pin    = PIN_3;
+    pinCfg.func   = PINSEL_FUNC_01;   // funcion UART0 RXD
+    PINSEL_ConfigPin(&pinCfg);
+
+    UART_CFG_T uartCfg;
+    uartCfg.baudRate = 9600;
+    uartCfg.parity   = UART_PARITY_NONE;
+    uartCfg.dataBits  = UART_DBITS_8;
+    uartCfg.stopBits  = UART_STOPBIT_1;
+    UART_Init(UART0, &uartCfg);
+
+    UART_FIFO_CFG_T fifoCfg;
+    fifoCfg.resetRxBuf = ENABLE;
+    fifoCfg.resetTxBuf = ENABLE;
+    fifoCfg.dmaMode    = DISABLE;
+    fifoCfg.level      = UART_FIFO_TRGLEV0;
+    UART_FIFOConfig(UART0, &fifoCfg);
+
+    UART_IntConfig(UART0, UART_INT_RBR, ENABLE);
+    NVIC_EnableIRQ(UART0_IRQn);
+
+    UART_TxEnable(UART0);
+}
+
+void enviarUART(void){
+
+    char msg[40];
+     int i = 0;
+     const char* sufijo = ",M:MANUAL\r\n";
+
+     msg[i++] = '$';
+     msg[i++] = 'S'; msg[i++] = '1'; msg[i++] = ':';
+     i += itoa_simple(angulo1, msg + i);
+     msg[i++] = ',';
+     msg[i++] = 'S'; msg[i++] = '2'; msg[i++] = ':';
+     i += itoa_simple(angulo2, msg + i);
+
+     if(Estado == 1){
+    	 sufijo = ",M:MANUAL\r\n";
+     }else{
+    	 sufijo = ",M:AUTO\r\n";
+     }
+     const char* p = sufijo;
+     while (*p) msg[i++] = *p++;
+     msg[i] = '\0';
+
+     UART_Send(UART0, (uint8_t*)msg, i, BLOCKING);
+}
+
+
+
+void configDMA(void){
+	GPDMA_Init();
+	GPDMA_Channel_CFG_T dmaCfg;
+	dmaCfg.channelNum=0;
+	dmaCfg.transferSize=1024;
+	dmaCfg.type=GPDMA_P2M;
+	dmaCfg.srcMemAddr=(uint32_t)&LPC_ADC->ADGDR;
+	dmaCfg.dstMemAddr=(uint32_t)BufferDMA;
+	dmaCfg.srcConn=GPDMA_ADC;
+	dmaCfg.dstConn=0;
+	dmaCfg.src.burst=GPDMA_BSIZE_1;
+	dmaCfg.src.width=GPDMA_WORD;
+	dmaCfg.src.increment=0;
+	dmaCfg.dst.burst=GPDMA_BSIZE_1;
+	dmaCfg.dst.width=GPDMA_WORD;
+	dmaCfg.dst.increment=ENABLE;
+	dmaCfg.intTC=DISABLE;
+	dmaCfg.intErr=DISABLE;
+	dmaCfg.linkedList=(uint32_t)&lli;
+	GPDMA_SetupChannel(&dmaCfg);
+	GPDMA_ChannelStart(GPDMA_CH_0);
+
+}
+
+
+void actualizarADC(void){
+    for (int i = 0; i < 1024; i++){
+        uint32_t dato = BufferDMA[i];
+
+        if (dato & (1UL << 31))        {
+            uint16_t adc   = (dato >> 4) & 0xFFF;
+            uint8_t  canal = (dato >> 24) & 0x07;
+
+            if (canal == 0)
+                convertido1 = adc;
+            else if (canal == 3)
+                convertido2 = adc;
+        }
+    }
+}
+
+
+
+void actualizarServo1(void){
+    PWM1 = 500 + ((uint32_t)convertido1 * 2000) / 4095;
+
+    TIM_UpdateMatchValue(LPC_TIM0, TIM_MATCH_0, PWM1);
+}
+
+void actualizarServo2(void){
+    PWM2 = 500 + ((uint32_t)convertido2 * 2000) / 4095;
+
+    TIM_UpdateMatchValue(LPC_TIM2, TIM_MATCH_0, PWM2);
+}
+
+
+int main(void){
+	lli.srcAddr = (uint32_t)&LPC_ADC->ADGDR;
+	lli.dstAddr = (uint32_t)BufferDMA;
+	lli.nextLLI = (uint32_t)&lli;
+	lli.control = 1024 | (2 << 18) | (2 << 21) | (1 << 27);
+	configPIN();
+	confiEINT();
+	configADC();
+	configDMA();
+	configUART();
+	configTimer();
+	while(1){
+		if(Estado == 1){
+			actualizarADC();
+			actualizarServo1();
+			actualizarServo2();
+			angulo1 = ((uint32_t)(PWM1 - 500) * 180) / 2000;
+			angulo2 = ((uint32_t)(PWM2 - 500) * 180) / 2000;
+			for(volatile int i=0; i<5000000; i++){
+				if(i == 4999999){
+				enviarUART();}
+			}
+		}else{
+			for(volatile int i = 0; i < 5000000; i++);
+			GPIO_TogglePins(PORT_0, PIN_4);
+		}
+	}
 }
 
 
@@ -316,113 +433,6 @@ void TIMER2_IRQHandler(void){
 	    }
 }
 
-void TIMER1_IRQHandler(void){
-	TIM_ClearIntPending(LPC_TIM1, TIM_MR0_INT);
-
-	/*ADC_StartCmd(ADC_START_NOW);
-
-	while(!ADC_ChannelGetStatus(ADC_CHANNEL_0, ADC_DATA_DONE));
-	while(!ADC_ChannelGetStatus(ADC_CHANNEL_3, ADC_DATA_DONE));
-
-	convertido1 = ADC_ChannelGetData(ADC_CHANNEL_0);
-	convertido2 = ADC_ChannelGetData(ADC_CHANNEL_3);*/
-
-	ADC_ChannelDisable(ADC_CHANNEL_3);
-	ADC_ChannelEnable(ADC_CHANNEL_0);
-
-	ADC_StartCmd(ADC_START_NOW);
-
-	while(!ADC_ChannelGetStatus(ADC_CHANNEL_0, ADC_DATA_DONE));
-	convertido1 = ADC_ChannelGetData(ADC_CHANNEL_0);
-
-	ADC_ChannelDisable(ADC_CHANNEL_0);
-	ADC_ChannelEnable(ADC_CHANNEL_3);
-
-	ADC_StartCmd(ADC_START_NOW);
-
-	while(!ADC_ChannelGetStatus(ADC_CHANNEL_3, ADC_DATA_DONE));
-	convertido2 = ADC_ChannelGetData(ADC_CHANNEL_3);
-
-	calcularPWMServo1();
-	calcularPWMServo2();
-
-	angulo1 = (convertido1 * 360) / 4095;
-	angulo2 = (convertido2 * 360) / 4095;
-
-	enviarUART();
-}
-
-
-void calcularPWMServo1(){
-	/*ADC_StartCmd(ADC_START_NOW);
-	while(!ADC_ChannelGetStatus(ADC_CHANNEL_0, ADC_DATA_DONE));
-	convertido = ADC_ChannelGetData(ADC_CHANNEL_0);*/
-    if(convertido1<455){
-    	PWM1 = 500;
-    }else if(convertido1>=455 && convertido1<910){
-    	PWM1 = 722;
-    }else if(convertido1>=910 && convertido1<1365){
-    	PWM1 = 944;
-    }else if(convertido1>=1365 && convertido1<1820){
-    	PWM1 = 1166;
-    }else if(convertido1>=1820 && convertido1<2275){
-    	PWM1 = 1388;
-    }else if(convertido1>=2275 && convertido1<2730){
-    	PWM1 = 1610;
-    }else if(convertido1>=2730 && convertido1<3185){
-    	PWM1 = 1832;
-    }else if(convertido1>=3185 && convertido1<3640){
-    	PWM1 = 2054;
-    }else if(convertido1>=3640 && convertido1<3950){
-    	PWM1 = 2276;
-    }else if(convertido1>=3950){
-    	PWM1 = 2500;
-    }
-
-    TIM_ResetCounter(LPC_TIM0);
-    TIM_UpdateMatchValue(LPC_TIM0, TIM_MATCH_0, PWM1);
-}
-
-
-void calcularPWMServo2(){/*
-	ADC_StartCmd(ADC_START_NOW);
-	while(!ADC_ChannelGetStatus(ADC_CHANNEL_3, ADC_DATA_DONE));
-	convertido = ADC_ChannelGetData(ADC_CHANNEL_3);*/
-    if(convertido2<455){
-    	PWM2 = 500;
-    }else if(convertido2>=455 && convertido2<910){
-    	PWM2 = 722;
-    }else if(convertido2>=910 && convertido2<1365){
-    	PWM2 = 944;
-    }else if(convertido2>=1365 && convertido2<1820){
-    	PWM2 = 1166;
-    }else if(convertido2>=1820 && convertido2<2275){
-    	PWM2 = 1388;
-    }else if(convertido2>=2275 && convertido2<2730){
-    	PWM2 = 1610;
-    }else if(convertido2>=2730 && convertido2<3185){
-    	PWM2 = 1832;
-    }else if(convertido2>=3185 && convertido2<3640){
-    	PWM2 = 2054;
-    }else if(convertido2>=3640 && convertido2<3950){
-    	PWM2 = 2276;
-    }else if(convertido2>=3950){
-    	PWM2 = 2500;
-    }
-
-    TIM_ResetCounter(LPC_TIM2);
-    TIM_UpdateMatchValue(LPC_TIM2, TIM_MATCH_0, PWM2);
-}
-
-
-void EINT0_IRQHandler(void){
-	EXTI_ClearFlag(EXTI_EINT0);
-    if(totalPosiciones < 2048){
-        PosicionesGuar1[totalPosiciones] = (uint32_t)PWM1;
-        PosicionesGuar2[totalPosiciones] = (uint32_t)PWM2;
-        totalPosiciones++;
-    }
-}
 
 void EINT1_IRQHandler(void){
 	EXTI_ClearFlag(EXTI_EINT1);
@@ -431,46 +441,27 @@ void EINT1_IRQHandler(void){
 	//GPIO_TogglePins(PORT_0, 1 << 2);
 }
 
-void configUART(void) {
-    UART_CFG_T uartCfg;
-    uartCfg.baudRate = 9600;
-    uartCfg.parity = UART_PARITY_NONE;
-    uartCfg.dataBits = UART_DBITS_8;
-    uartCfg.stopBits = UART_STOPBIT_1;
-	UART_Init(UART0, &uartCfg);
+void EINT2_IRQHandler(void){
+	EXTI_ClearFlag(EXTI_EINT2);
 
-	UART_FIFO_CFG_T fifoCfg;
-	fifoCfg.resetRxBuf = ENABLE;
-	fifoCfg.resetTxBuf = ENABLE;
-	fifoCfg.dmaMode = DISABLE;
-	fifoCfg.level = UART_FIFO_TRGLEV0;
-	UART_FIFOConfig(UART0, &fifoCfg);
-
-	UART_TxEnable(UART0);
+    //PosicionesGuar1[index]=PWM1;
+    //PosicionesGuar2[index]=PWM2;
+    index++;
+    if(index==4096){
+    	index=0;
+    }
 }
 
-void enviarUART(void)
-{
-    char msg[40];
+void UART0_IRQHandler(void){
+    NVIC_ClearPendingIRQ(UART0_IRQn);
 
-    sprintf(msg,
-            "$S1:%d,S2:%d,M:MANUAL\n",
-            angulo1,
-            angulo2);
+	uint32_t valorUART = UART_ReceiveByte(UART0);
 
-    UART_Send(UART0,
-              (uint8_t*)msg,
-              strlen(msg),
-              BLOCKING);
+    if (valorUART == '1') {
+    	Estado = 1;
+    } else if (valorUART == '0') {
+        Estado = 0;
+    }
 }
 
-void configDAC(void){
-	//nada
-}
-void configDMA(void){
-	//nada
-}
-//void configUART(void){
-	//nada
-//}
 
